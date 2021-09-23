9c1c8007d3fe6b41676dd8f50ca6cf8b817c4dc7
Maya Borrero <mlbsindia@gmail.com>
2021-09-23 14:38:52 -0400

    commit tree d8eb3a42209ffc5ab92a908dfee12f631d27498d

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(2,4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    const fareMultiplier = multiplier => 5 * multiplier;
+    return fareMultiplier;
+}
+
+function fareDoubler (fare) {
+    let doubledFare = fare * 2;
+    return doubledFare;
+}
+
+function fareTripler (fare) {
+    let tripledFare = fare * 3;
+    return tripledFare;
+}
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers);
+}
